44c063a5adbd5552a89563cb590db13e8e57fab1
sanjaydraws <sprajapat8331@gmail.com>
2021-10-12 00:33:46 +0530

    initial setup

---
+++ app/src/main/java/com/sanjay/galleryphotos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sanjay.galleryphotos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
